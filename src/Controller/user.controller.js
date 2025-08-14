10a979ecfa63a427ed330b3f898369872187da4c
ishira-web <ishira.pahasara@icloud.com>
2025-08-14 23:17:53 +0530

    feat: Implement createUser function to handle user registration

--- src/Controller/user.controller.js
+++ src/Controller/user.controller.js
@@ -1,3 +1,7 @@
 import User from "../Model/user.model.js";
 
 // create new user
+export const createUser = async (req, res) => {
+    const { fullName, email, phoneNumber, password, role, addressLine1, houseNumber, city, aTaxNumber, postalCode } = req.body;
+
+}
