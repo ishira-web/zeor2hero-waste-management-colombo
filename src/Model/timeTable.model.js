080bb6cc315475ae9de14717d9a234fbb3957558
ishira-web <ishira.pahasara@icloud.com>
2025-08-16 01:16:55 +0530

    feat: Add TimeTable model with schema for collection scheduling

---
+++ src/Model/timeTable.model.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const  timeTableSchema = mongoose.Schema({
+    timeTableID: { type: String, required: true, unique: true, default: function() { return `#TT${Math.floor(1000 + Math.random() * 9000)}` } },
+    collectorID: { type: mongoose.Schema.Types.ObjectId, ref: 'Collectors', required: true },
+    collectionDay: { type: String, required: true },
+    collectionTime: { type: String, required: true },
+    collectionLocation: { type: String, required: true },
+    isActive: { type: Boolean, default: true }
+}, { timestamps: true });
+const TimeTable = mongoose.model("TimeTable", timeTableSchema);
+export default TimeTable;
