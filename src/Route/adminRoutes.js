affc6800c834b408a00421a0d95f9fbea16aa101
ishira-web <ishira.pahasara@icloud.com>
2025-08-20 13:00:41 +0530

    feat: Add getAllAdmins route to adminRouter for retrieving all admin users

--- src/Route/adminRoutes.js
+++ src/Route/adminRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { adminCreate } from "../Controller/admin.controller.js";
+import { adminCreate, getAllAdmins } from "../Controller/admin.controller.js";
 import { protect } from '../Middleware/authMiddleware.js';
 
 const adminRouter = express.Router();
 
 adminRouter.post('/create',protect(["admin"]), adminCreate);
-
+adminRouter.get('/allAdmin',protect(["admin"]),getAllAdmins)
 export default adminRouter;
