62734c9f66734c907cb14fde1a4e19499708d13e
ishira-web <ishira.pahasara@icloud.com>
2025-08-20 14:28:18 +0530

    feat: Add admin protection to getAllUsers route in userRouter

--- src/Route/userRoutes.js
+++ src/Route/userRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { createUser, deleteUserById, getAllUsers, getUserById, updateUserById } from '../Controller/user.controller.js';
+import { protect } from '../Middleware/authMiddleware.js';
 
 export const userRouter = express.Router();
 
 userRouter.post('/create',createUser);
-userRouter.get('/all',getAllUsers);
+userRouter.get('/all',protect(["admin"]), getAllUsers);
 userRouter.get('/:id',getUserById);
 userRouter.put('/update/:id',updateUserById);
 userRouter.delete('/delete/:id', deleteUserById); 
+
