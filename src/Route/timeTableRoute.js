6c971eaa5d87a1b1e6951f19f118d5cd578a2bb1
ishira-web <ishira.pahasara@icloud.com>
2025-08-21 00:23:29 +0530

    feat: Add time table routes for creating and retrieving time tables

---
+++ src/Route/timeTableRoute.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { createTimeTable, getActiveTimeTables, getAllTimeTables, getTimeTableById, updateTimeTableById } from '../Controller/timeTable.controller.js';
+
+export const timeRoute = express.Router();
+
+timeRoute.post('/create',createTimeTable);
+timeRoute.get('/all', getAllTimeTables);
+timeRoute.get('/:id', getTimeTableById);
+timeRoute.put('/:id', updateTimeTableById);
+timeRoute.get('/gettimetable',getActiveTimeTables);
+
+
