451a280460d164fd6075779df40bdb3204091fdb
ishira-web <ishira.pahasara@icloud.com>
2025-08-20 17:24:54 +0530

    feat: Increase file size limit for image uploads to 20MB and remove admin protection from getAllCollectors route

--- src/Config/Multer.js
+++ src/Config/Multer.js
@@ -14,7 +14,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 }, 
+  limits: { fileSize: 20 * 1024 * 1024 }, 
 });
 
 export default upload;
--- src/Route/collectorRoutes.js
+++ src/Route/collectorRoutes.js
@@ -5,7 +5,7 @@ import { protect } from '../Middleware/authMiddleware.js';
 const router = express.Router();
 
 router.post('/createcollector',upload.fields([{ name: "nicImage", maxCount: 1 },{ name: "profilePicture", maxCount: 1 },]),protect(["admin"]), createCollector)
-router.get('/getcollectors',protect(["admin"]),getAllCollectors);
+router.get('/getcollectors',getAllCollectors);
 router.get('/getcollector/:id',protect(["admin","collector"]),getCollectorById); 
 router.delete('/deletecollector/:id',protect(["admin"]),deleteCollectorById);
 router.put('/updatecollector/:id',protect(["admin"]),updateCollectorById);
