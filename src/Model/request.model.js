0e34590cc47c8478735f1688bc74c667822786ad
ishira-web <ishira.pahasara@icloud.com>
2025-08-21 08:41:22 +0530

    feat: Add request model with schema definition for handling requests

---
+++ src/Model/request.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const requestSchema = mongoose.Schema({
+    requestID: { type: String, required: true, unique: true, default: function() { return `#REQ${Math.floor(1000 + Math.random() * 9000)}` } },
+    collectorID: { type: String, required: true },
+    userName: { type: String, required: true },
+    dwellerID: { type: String, required: true },
+    dwellerAddress: { type: String, required: true },
+    requestType: { type: String, required: true, enum: ['General', 'Urgent', 'Emergency'] },
+    requestStatus: { type: String, required: true, enum: ['Pending', 'In Progress', 'Completed', 'Cancelled'], default: 'Pending' },
+    requestDate: { type: Date, default: Date.now },
+    requestTime: { type: String, required: true },
+    additionalNotes: { type: String, default: '' },
+}, { timestamps: true });
+const Request = mongoose.model("Request", requestSchema);
+export default Request;
